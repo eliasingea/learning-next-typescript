e3211842a0dc8c6ff5ec784d5124bb39da1cd2b0
Elias Ingea <eliasingea@gmail.com>
2022-10-26 10:52:49 -0400

    start the baseline

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="text-red-200">
-      Hello
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="p-2"></div>
+      <div className="text-2xl text-center">Which Pokemon is rounder</div>
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div className="w-16 h-16 bg-red-200">this</div>
+        <div className="p-8">Vs</div>
+        <div className='w-16 h-16 bg-red-200'>that</div>
+      </div>
     </div>
   )
 }
